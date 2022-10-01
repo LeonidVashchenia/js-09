73bca818c51eccc064d5b3c73909e4d9f704c0d8
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-10-01 19:25:40 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButtunRef = document.querySelector('button[data-start]');
+const stopButtunRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+let timerId = null;
+
+const chengerBodyBgrColor = () => {
+  startButtunRef.disabled = true;
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  // console.log(startButtunRef);
+};
+
+startButtunRef.addEventListener('click', chengerBodyBgrColor);
+stopButtunRef.addEventListener('click', () => {
+  startButtunRef.disabled = false;
+  clearInterval(timerId);
+});
