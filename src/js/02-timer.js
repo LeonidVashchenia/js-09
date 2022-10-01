69811a4b96feee0b3c8ecbba806abb52c3b17084
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-10-02 00:38:12 +0300

    task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,24 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const inputRef = document.querySelector('#datetime-picker');
 console.log(inputRef);
 const buttonRef = document.querySelector('button[data-start]');
 console.log(buttonRef);
-console.log('ffvv');
+
+let timeN = null;
+console.log(timeN);
+buttonRef.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  // defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    timeN = selectedDates[0];
+    console.log('timeN', timeN);
+  },
+};
+
+const calendar = flatpickr(inputRef, options);
