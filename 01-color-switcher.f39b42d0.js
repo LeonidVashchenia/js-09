bc4ad90100659dcf9b521fce7a019d35e20d7a0e
LeonidVashchenia <46425191+LeonidVashchenia@users.noreply.github.com>
2022-10-01 16:26:18 +0000

    Deploying to gh-pages from @ LeonidVashchenia/js-09@73bca818c51eccc064d5b3c73909e4d9f704c0d8 🚀

---
+++ 01-color-switcher.f39b42d0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{t.disabled=!0,r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(r)}));
+//# sourceMappingURL=01-color-switcher.f39b42d0.js.map
